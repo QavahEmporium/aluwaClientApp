2fdef39ff959568cb76e94f3e9088d2af30762f9
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-08-12 22:09:58 +0200

    refactor: updated cart items number

--- components/cart/cart-icon.tsx
+++ components/cart/cart-icon.tsx
@@ -5,7 +5,7 @@ import { useCart } from "@/context/CartContext";
 
 export function CartIcon() {
   const { cart, openCart } = useCart();
-  const itemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const badge = cart?.length || 0;
 
   return (
     <button
@@ -13,9 +13,9 @@ export function CartIcon() {
       className="relative p-2 rounded hover:bg-gray-100"
     >
       <ShoppingCart className="w-6 h-6" />
-      {itemCount > 0 && (
+      {badge > 0 && (
         <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
-          {itemCount}
+          {badge}
         </span>
       )}
     </button>
