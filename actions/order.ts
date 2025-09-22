62188bb691461b8e824fbc35619578f09b93ffc5
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-09-22 10:51:34 +0200

    refactor: updated the creating payment URL

--- actions/order.ts
+++ actions/order.ts
@@ -40,12 +40,12 @@ export async function createOrderAction(
     }
 
     // 🔹 create order
-    /* const order = await createOrder(
+    const order = await createOrder(
       { name, email, address, city, postalCode, country },
       cart
-    ); */
-    /* if (order)  */
-    url = await createPaymentUrl(1000.34, "Aluwa HairCare Product");
+    );
+
+    url = await createPaymentUrl(order?.totalAmount!, "Aluwa HairCare Product");
   } catch (error) {
     console.error("Error creating order:", error);
     return {
@@ -54,41 +54,45 @@ export async function createOrderAction(
   }
 
   // ✅ redirect to order success page
-  console.log({ url });
   redirect(url);
 }
 
 export async function createPaymentUrl(amount: number, product_name: string) {
   const user = await getSessionUser();
-  // ✅ Validate input with zod schema
-  const pfHost =
-    process.env.PAYFAST_MODE === "sandbox"
-      ? process.env.PAYFAST_SANDBOX_URL!
-      : process.env.PAYFAST_LIVE_URL!;
 
+  const email =
+    process.env.PAYFAST_MODE === "sandbox"
+      ? "admin@theugatour.com"
+      : user?.email;
+  const names = user?.name.split(" ");
+  const first = names[0];
+  const last = names.length > 1 ? names[1] : "";
+
+  // ✅ Validate input with zod schema
   const data: Record<string, string> = {
     merchant_id: process.env.PAYFAST_MERCHANT_ID!,
     merchant_key: process.env.PAYFAST_MERCHANT_KEY!,
     return_url: `${process.env.SITE_URL!}/${process.env.PAYFAST_RETURN_URL!}`,
     cancel_url: `${process.env.SITE_URL!}/${process.env.PAYFAST_CANCEL_URL!}`,
     notify_url: `${process.env.SITE_URL!}/${process.env.PAYFAST_NOTIFY_URL!}`,
+    name_first: first,
+    name_last: last,
+    email_address: email, // sandbox email
     amount: amount.toFixed(2),
     item_name: product_name,
-    email_address: user?.email,
+    item_description: product_name,
   };
 
-  // Build query string
-  let queryString = Object.entries(data)
+  // 3️⃣ Build final encoded query string with signature
+  const encodedQuery = Object.entries(data)
+    .sort(([a], [b]) => a.localeCompare(b))
     .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
     .join("&");
 
-  // Generate signature if passphrase exists
-  if (process.env.PAYFAST_PASSPHRASE) {
-    queryString += `&passphrase=${encodeURIComponent(
-      process.env.PAYFAST_PASSPHRASE
-    )}`;
-  }
+  const pfHost =
+    process.env.PAYFAST_MODE === "sandbox"
+      ? process.env.PAYFAST_SANDBOX_URL!
+      : "https://www.payfast.co.za";
 
-  const signature = crypto.createHash("md5").update(queryString).digest("hex");
-  return `${pfHost}/eng/process?${queryString}&signature=${signature}`;
+  return `${pfHost}/eng/process?${encodedQuery}`;
 }
